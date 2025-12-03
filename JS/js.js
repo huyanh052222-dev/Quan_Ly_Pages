8af118aa413fbf8b03e4a2a0d8ce1fcc74e295bc
huyanh052222-dev <huyanh052222@gmail.com>
2025-12-03 22:07:14 +0700

    version 1.3.3

---
+++ JS/js.js
@@ -0,0 +1,13 @@
+const boxes = document.querySelectorAll('.box');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+boxes.forEach(box => observer.observe(box));
